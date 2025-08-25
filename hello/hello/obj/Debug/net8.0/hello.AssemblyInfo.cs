cdf25cb54427424e8ef4b0ca34e9fc5de82cd49a
widedoueslati82 <167637733+widedoueslati82@users.noreply.github.com>
2025-08-25 15:56:57 -0400

    modification de branche de branche
    modification de branche

--- hello/hello/obj/Debug/net8.0/hello.AssemblyInfo.cs
+++ hello/hello/obj/Debug/net8.0/hello.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6ceea0968f046d7874207741f630d49787b127")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
